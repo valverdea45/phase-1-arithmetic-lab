e553c772076046545d881af4d233c50a8dc92c86
Adrian <adrianvalverde45@gmail.com>
2022-09-20 13:43:47 -0400

    commit tree 4ae00b393350acead014b35ac97f064188dcf4a1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random()* 10) + 1; 
+
+const num3 = 14
+const num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(1,2,3,16,18,19,10,20)
